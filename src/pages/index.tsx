0942cfcb176711d170826fcb8954c2017556756f
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-20 14:04:41 -0300

    Gerando SSG no Next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
-const Home = () => {
+export default function Home() {
   return (
-    <h1>Index</h1>
+    <>
+      <h1>Index</h1>
+    </>
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8 // A cada 8 horas o site irá revalidar
+  }
+}
