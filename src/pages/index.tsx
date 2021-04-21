7534f17d789ed002bd7b389c2ccd3c80091bb6f4
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-21 10:05:29 -0300

    Criando utils e funçao de conversao da duraçaod o podcast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 // TIPAGEM
 import { GetStaticProps } from 'next'
-
 interface Episode {
   id: string;
   title: string;
   members: string;
+  published_at: string;
 }
 interface HomeProps {
-  episodes: Episode[] // Isso significa que episodes é um array, também poderia ser assim "Array<Episode>" sendo que dentro do menor e maior fica o tipo do dado que tem no array
+  episodes: Episode[] // Isso significa que Episode é um array, também poderia ser assim "Array<Episode>" sendo que dentro do menor e maior fica o tipo do dado que tem no array
 }
+
+// AXIOS
+import api from '../services/api'
+
+// DATE FNS
+import { format, parseISO } from 'date-fns'
 
 export default function Home(props: HomeProps) {
   return (
@@ -19,8 +25,23 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: episode.published_at
+    }
+  })
 
   return {
     props: {
