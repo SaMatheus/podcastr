062c8821b52c6a798840532c10f3d3304cace643
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-21 10:13:28 -0300

    Finalizado conversao da duraçao para string

--- src/utils/convertDurationToTimeString.ts
+++ src/utils/convertDurationToTimeString.ts
@@ -1,13 +1,13 @@
 const convertDurationToTimeString = (duration: number) => {
-  const hours = Math.floor(duration / 3600)
-  const minutes = Math.floor((duration % 3600) / 60)
-  const seconds = duration % 60
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = duration % 60;
 
   const timeString = [hours, minutes, seconds]
     .map(unit => String(unit).padStart(2, '0'))
-    .join(':')
+    .join(':');
 
-  return timeString
+  return timeString;
 }
 
 export default convertDurationToTimeString
