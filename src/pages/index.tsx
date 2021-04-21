a065f5f27eecc38073f92af933f16a152e841144
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-21 10:14:05 -0300

    Tipando dados da api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
+// STYLES
+import styles from '../styles/pages/home.module.scss'
+
 // TIPAGEM
 import { GetStaticProps } from 'next'
 interface Episode {
   id: string;
   title: string;
   members: string;
-  published_at: string;
+  publishedAt: string;
+  thumbnail: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
 }
 interface HomeProps {
   episodes: Episode[] // Isso significa que Episode é um array, também poderia ser assim "Array<Episode>" sendo que dentro do menor e maior fica o tipo do dado que tem no array
@@ -15,12 +23,18 @@ import api from '../services/api'
 
 // DATE FNS
 import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+// UTILS
+import convertDurationToTimeString from '../utils/convertDurationToTimeString'
+
+
 
 export default function Home(props: HomeProps) {
   return (
-    <>
+    <div>
       <h1>Index</h1>
-    </>
+    </div>
   )
 }
 
@@ -39,13 +53,17 @@ export const getStaticProps: GetStaticProps = async () => {
       title: episode.title,
       thumbnail: episode.thumbnail,
       members: episode.members,
-      publishedAt: episode.published_at
+      publishedAt: format(parseISO(episode.published_at), 'd MMM YY', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      ulr: episode.file.url,
     }
   })
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8 // A cada 8 horas o site irá revalidar
   }
