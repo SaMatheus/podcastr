eb4537c0d9e439e74efd41707f32f8cf02f6ffe2
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-21 09:37:42 -0300

    Crianco axios service

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
+// TIPAGEM
+import { GetStaticProps } from 'next'
+
+interface Episode {
+  id: string;
+  title: string;
+  members: string;
+}
+interface HomeProps {
+  episodes: Episode[] // Isso significa que episodes é um array, também poderia ser assim "Array<Episode>" sendo que dentro do menor e maior fica o tipo do dado que tem no array
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Index</h1>
@@ -6,7 +18,7 @@ export default function Home() {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch('http://localhost:3333/episodes')
   const data = await response.json()
 
