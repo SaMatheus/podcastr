b18ef620bf733ee65345cd733ace9ea7423e5ac0
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-26 09:08:30 -0300

    Estilizando informaçoes do player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,10 @@
 // STYLES
 import styles from './styles.module.scss'
 
+// SLIDER
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+
 // NEXT
 import Image from 'next/image'
 
@@ -42,25 +46,33 @@ export const Player = () => {
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
 
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type="button" className={styles.playButton}>
+          <button type="button" className={styles.playButton} disabled={!episode}>
             <img src="/play.svg" alt="Tocar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar proxima" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
